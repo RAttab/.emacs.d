a3bc53be27556fbf288c7b9b10bcb9a1be02df20
Rémi Attab <remi@recoset.com>
2012-05-01 10:46:38 -0400

    Fixed the project.el file.

--- config/project.el
+++ config/project.el
@@ -1,35 +1,9 @@
 
-(ede-cpp-root-project "dropbox"
-	:name "Dropbox Chalenges"
-	:file "~/code/dropbox/CMakeLists.txt"
-	:include-path '("/src")
-)
-
-
-(ede-cpp-root-project "libyarn"
-		    :name "libyarn"
-		    :file "~/code/yarn/libyarn/Makefile"
-		    :include-path '("/src")
-		    ;; :system-include-path '("/full/path/here")
-		    ;; :spp-table '(("PREPROC_DEFINE", "VALUE"))
-)
-
-(ede-cpp-root-project "yarnb"
-		    :name "yarnb"
-		    :file "~/code/yarn/yarnb/Makefile"
-		    :include-path '("/src"
-				    "../../libyarn/src"
-				   )
-		    ;; :system-include-path '("/full/path/here")
-		    ;; :spp-table '(("PREPROC_DEFINE", "VALUE"))
-)
-
-(ede-cpp-root-project "yarnc"
-		    :name "yarnc"
-		    :file "~/code/yarn/yarnc/Makefile"
-		    :include-path '("./include"
-				    "./lib/Yarn")
-		    :system-include-path '("~/code/llvm/llvm-src/include/")
-		    ;; :spp-table '(("PREPROC_DEFINE", "VALUE"))
-)
+;; (ede-cpp-root-project "libyarn"
+;; 		    :name "libyarn"
+;; 		    :file "~/code/yarn/libyarn/Makefile"
+;; 		    :include-path '("/src")
+;; 		    ;; :system-include-path '("/full/path/here")
+;; 		    ;; :spp-table '(("PREPROC_DEFINE", "VALUE"))
+;; )
 
