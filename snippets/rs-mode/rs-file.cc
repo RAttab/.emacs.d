9c3915ef067c4b87c9ef79cea47d8b4644c4ee7a
Rémi Attab <remi@recoset.com>
2012-07-07 11:39:40 -0400

    Added rs-mode along with file templates.

---
+++ snippets/rs-mode/rs-file.cc
@@ -0,0 +1,23 @@
+# -*- mode: snippet -*-
+# name: cc
+# key: rs-file
+# expand-env: ((yas/indent-line 'fixed))
+# --
+/** `(file-name-nondirectory (buffer-file-name))`                                 -*- C++ -*-
+    Rémi Attab, `(format-time-string "%d %b %Y")`
+    Copyright (c) `(format-time-string "%Y")` Recoset.  All rights reserved.
+
+    ${1:Description}
+
+*/
+
+#include "`(file-name-nondirectory (file-name-sans-extension (buffer-file-name)))`.h"
+
+using namespace std;
+using namespace ML;
+
+namespace Recoset {
+
+$0
+
+} // namepsace Recoset
