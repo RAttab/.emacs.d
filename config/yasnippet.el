0234345f1ace813dbcc5eae7eeb93e5b80dc78e6
Rémi Attab <remi@recoset.com>
2012-07-07 11:11:41 -0400

    enabled ido for yassnippet completion

--- config/yasnippet.el
+++ config/yasnippet.el
@@ -2,7 +2,7 @@
 (require 'yasnippet)
 (yas/global-mode 1)
 
-(setq yas/root-directory '("~/.emacs.d/snippets"))
-			   ;; "~/.emacs.d/vendor/yasnippet/snippets"))
+(setq yas/prompt-functions '(yas/ido-prompt))
 
+(setq yas/root-directory '("~/.emacs.d/snippets"))
 (mapc 'yas/load-directory yas/root-directory)
