a783d2e1571c5ed89e135673bcb4d0dfa2c3e053
Rémi Attab <remi@recoset.com>
2012-07-07 22:48:16 -0400

    Added the sampled project.el.

---
+++ config/sample.project.el
@@ -0,0 +1,9 @@
+
+;; (ede-cpp-root-project "libyarn"
+;; 		    :name "libyarn"
+;; 		    :file "~/code/yarn/libyarn/Makefile"
+;; 		    :include-path '("/src")
+;; 		    ;; :system-include-path '("/full/path/here")
+;; 		    ;; :spp-table '(("PREPROC_DEFINE", "VALUE"))
+;; )
+
