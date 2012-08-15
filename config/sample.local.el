82e0934e41e61d1dbe5fe326349e8325f67995cc
Rémi Attab <remi.attab@gmail.com>
2012-08-15 10:45:30 -0400

    Expanded the local.el sample.

--- config/sample.local.el
+++ config/sample.local.el
@@ -1,8 +1,15 @@
+;; Frame position
+(setq initial-frame-alist
+      '((top . 0) (left . 0) (width . 200) (height . 100)))
 
 ;; Set the background to black.
-(set-face-attribute 'default nil 
+(set-face-attribute 'default nil
 		    :background "black"
 		    :font "Bitstream Vera Sans Mono-8")
+
+;; Foreground yellow in base.el changes the cursor color. Calling this
+;; before the previous line causes the cursor to disapear.
+(set-cursor-color "white")
 
 ;; Compilation mode
 (setenv "PATH"
@@ -11,7 +18,7 @@
 		":/home/remi/local/bin"))
 (setenv "LD_LIBRARY_PATH" "/home/remi/local/lib")
 
-(setq compilation-search-path 
+(setq compilation-search-path
       '(nil ;; Default dir
 	"/home/remi/code"))
 
