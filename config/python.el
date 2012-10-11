58318a9074202431136e7ab47a52ee9e9f4d5887
Rémi Attab <remi@datacratic.com>
2012-10-11 07:12:36 -0400

    Pimped the python hook with auto-fill-mode

--- config/python.el
+++ config/python.el
@@ -3,5 +3,8 @@
 ;; Could use some extra configuration.
 
 ;; Automatic indentation on newline
-(add-hook 'python-mode-hook '
-	  (lambda () (define-key python-mode-map "\C-m" 'newline-and-indent)))
+(defun my-python-hook ()
+  (define-key python-mode-map "\C-m" 'newline-and-indent)
+  (utils/enable-auto-fill 80 nil))
+
+(add-hook 'python-mode-hook 'my-python-hook)
