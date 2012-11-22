b2500658c99c7e8c4ae2a644a48a15030a0d5138
Rémi Attab <remi@recoset.com>
2012-11-22 11:59:33 -0500

    Python file header snippets.

---
+++ snippets/rs-mode/rs-file.py
@@ -0,0 +1,13 @@
+# -*- mode: snippet -*-
+# name: py
+# key: rs-file
+# expand-env: ((yas/indent-line 'fixed))
+# --
+#-------------------------------------------------------------- -*- python -*- #
+# `(file-name-nondirectory (buffer-file-name))`
+# Remi Attab, `(format-time-string "%d %b %Y")`
+# Copyright (c) `(format-time-string "%Y")` Datacratic.  All rights reserved.
+#
+# ${1:Description}
+#------------------------------------------------------------------------------#
+$0
