dd13c780a04477cbc3b4e0c46800701c906acbe2
Rémi Attab <remi.attab@gmail.com>
2014-04-21 17:22:21 -0400

    Fixed tab width in go-mode.

--- config/go.el
+++ config/go.el
@@ -12,6 +12,7 @@
 (add-hook 'before-save-hook 'gofmt-before-save)
 
 (defun utils/go-mode-hook ()
+  (setq tab-width 4)
   (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
   (local-set-key (kbd "C-c i") 'go-goto-imports))
 (add-hook 'go-mode-hook 'utils/go-mode-hook)
