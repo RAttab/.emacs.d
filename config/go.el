cf326865280269990128c7b79cc24cda306bf3ed
Rémi Attab <remi.attab@gmail.com>
2014-04-21 20:23:55 -0400

    On save formatting is super annoying.

--- config/go.el
+++ config/go.el
@@ -9,10 +9,9 @@
 (setq load-path (cons "/usr/local/go/misc/emacs" load-path))
 (require 'go-mode-load)
 
-(add-hook 'before-save-hook 'gofmt-before-save)
-
 (defun utils/go-mode-hook ()
   (setq tab-width 4)
+  (local-set-key (kbd "C-c C-f") 'gofmt)
   (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
   (local-set-key (kbd "C-c i") 'go-goto-imports))
 (add-hook 'go-mode-hook 'utils/go-mode-hook)
