0eab831dd4f26ced3f0c23df6ee81e87b92ae375
Rémi Attab <remi@datacratic.com>
2014-05-30 13:56:45 -0400

    Tweaked the go configs.

--- config/go.el
+++ config/go.el
@@ -12,13 +12,17 @@
 (defun utils/go-is-sys () (when (file-exists-p utils/go-sys-path) t))
 (defun utils/go-is-usr () (when (file-exists-p utils/go-usr-path) t))
 
+(defun utils/go-load-path (path)
+  (setq load-path (cons path load-path)))
+
 (defun utils/go-load ()
-  (if (utils/go-is-sys) (setq load-path (cons utils/go-sys-path load-path))
-    (when (utils/go-is-usr) (setq load-path (cons utils/go-usr-path load-path)))))
+  (if (utils/go-is-sys) (utils/go-load-path utils/go-sys-path)
+    (when (utils/go-is-usr) (utils/go-load-path utils/go-usr-path))))
 
 
 (defun utils/go-mode-hook ()
   (setq tab-width 4)
+  ;; (add-hook 'before-save-hook 'gofmt-before-save) ;; Super annoying.
   (local-set-key (kbd "C-c C-f") 'gofmt)
   (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
   (local-set-key (kbd "C-c i") 'go-goto-imports))
