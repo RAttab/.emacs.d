b05d3fae0c2b8bbe1fcf4c5fe43397e7c31d4f9e
Rémi Attab <remi.attab@gmail.com>
2014-11-02 13:04:01 -0500

    python no-longer auto-fills.

--- config/python.el
+++ config/python.el
@@ -4,7 +4,6 @@
 
 ;; Automatic indentation on newline
 (defun my-python-hook ()
-  (define-key python-mode-map "\C-m" 'newline-and-indent)
-  (utils/enable-auto-fill 80 nil))
+  (define-key python-mode-map "\C-m" 'newline-and-indent))
 
 (add-hook 'python-mode-hook 'my-python-hook)
