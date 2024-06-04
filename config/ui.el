ad0c6e869365778da0bc1133b6473a5dbfe8f613
Rémi Attab <remi@marketops.com>
2024-06-04 10:08:47 -0400

    Remove scroll-bar-mode config

--- config/ui.el
+++ config/ui.el
@@ -45,7 +45,6 @@
 (setq inhibit-splash-screen t)
 (tool-bar-mode 0)
 (menu-bar-mode 0)
-(scroll-bar-mode -1)
 
 ;; Disable the stupid mouse wheel acceleration
 (setq mouse-wheel-progressive-speed nil)
